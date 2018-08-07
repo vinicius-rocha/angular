b6b6b255820c7ca01f397a4cbc79b061ef2e0179
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-06 21:40:34 -0300

    Criando nosso primeiro componente
    - Criando o nosso photo-component (ap-photo).

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent {
+    description = 'Leão';
+    url = 'http://elelur.com/data_images/mammals/lion/lion-05.jpg';
+}
