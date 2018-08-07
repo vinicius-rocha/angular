660adfbb4981652108ec3163b404690a10247345
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-07 14:24:18 -0300

    Organizando melhor nosso código
    - Criando um novo componente via angular cli.
    - Configurando aspas simples.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
     declarations: [
-        PhotoComponent
+        PhotoComponent,
+        PhotoListComponent
     ],
     imports: [
         HttpClientModule
-    ],
-    exports: [
-        PhotoComponent
     ]
 })
 
