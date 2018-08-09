ad83d3fcd19392f1cd8a89072abe7f049e26a5c8
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-09 14:04:11 -0300

    Componetizando o filtro search

---
+++ src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+    selector: 'ap-search',
+    templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnInit, OnDestroy{
+    debounce: Subject<string> = new Subject<string>();
+
+    ngOnInit(): void {
+       
+        this.debounce
+          .pipe(debounceTime(500))
+          /*.subscribe(filter => this.filter = filter);*/
+      }
+    
+      ngOnDestroy(): void {
+        this.debounce.unsubscribe();
+      }
+ }
