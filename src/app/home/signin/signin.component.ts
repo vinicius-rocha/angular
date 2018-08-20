9de170851a312817ef02bea4e98fdc05e5c5b946
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-20 16:34:37 -0300

    Redirecionando após login

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../core/auth/auth.service';
 
@@ -10,7 +11,7 @@ export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -26,13 +27,11 @@ export class SignInComponent implements OnInit {
         this.authService
             .authenticate(userName, password)
             .subscribe(() => {
-                console.log('autenticado')
+                this.router.navigate(['user', userName])
             }, err => {
                 console.log(err);
                 this.loginForm.reset();
                 alert('Invalid user name or password');
-            })
-            
-            ;
+            });
     }
 }
