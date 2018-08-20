e940da3dbae119c73b615bebb4e4a31cd0e0d5f3
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-20 11:13:37 -0300

    Validação de formulários

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
     declarations: [
         SignInComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
     ]
 })
 export class HomeModule { }
