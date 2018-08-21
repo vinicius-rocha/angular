b1567fdedb115b9aa96b90ba1b04fa21a5f1df26
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-21 16:43:52 -0300

    O componente de registro de usuários

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
 import { AuthGuard } from './core/auth/auth.guard';
+import { SignUpComponent } from './home/singup/signup.component';
 
 const routes: Routes = [
     {
@@ -15,6 +16,10 @@ const routes: Routes = [
         canActivate: [
             AuthGuard
         ]
+    },
+    {
+        path: 'signup',
+        component: SignUpComponent
     },
     {
         path: 'user/:userName', 
