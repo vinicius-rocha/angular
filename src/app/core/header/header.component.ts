af2c9c72282327c7fd4901dab33adb4edc659c3e
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-21 15:05:16 -0300

    Logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.serrvice';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ap-header',
@@ -11,7 +12,12 @@ export class HeaderComponent {
 
     user$: Observable<User>;
 
-    constructor(userService: UserService) {
+    constructor(private userService: UserService, private router: Router) {
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
