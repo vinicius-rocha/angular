dd4c9b958af5687034285319c0b49f499a0ac23d
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-21 12:05:59 -0300

    Separando as responsabilidades

---
+++ src/app/core/user/user.serrvice.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import * as jtw_decode from 'jwt-decode';
+
+import { User } from './user';
+import { TokenService } from '../token/token.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    private userSubject = new Subject<User>();
+
+    constructor(private tokenService: TokenService) {
+        this.tokenService.hasToken() && this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jtw_decode(token) as User;
+        this.userSubject.next(user);
+    }
+}
