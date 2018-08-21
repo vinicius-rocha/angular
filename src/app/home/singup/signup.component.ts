90ca132d8924afc70f39ce09992beccbeb781381
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-21 19:06:44 -0300

    Validador assíncrono

--- src/app/home/singup/signup.component.ts
+++ src/app/home/singup/signup.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from '../../shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent implements OnInit{
-    
+export class SignUpComponent implements OnInit {
+
     signupForm: FormGroup;
-    
-    constructor(private formBuilder: FormBuilder) { }
-        
+
+    constructor(private formBuilder: FormBuilder, private userNotTakenValidatorService: UserNotTakenValidatorService) { }
+
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
             email: [
-                '', 
+                '',
                 [
-                    Validators.required, 
+                    Validators.required,
                     Validators.email
                 ]
             ],
@@ -35,7 +36,8 @@ export class SignUpComponent implements OnInit{
                     lowerCaseValidator,
                     Validators.minLength(2),
                     Validators.maxLength(30)
-                ]
+                ],
+                this.userNotTakenValidatorService.checkUserNameTaken()
             ],
             password: [
                 '',
