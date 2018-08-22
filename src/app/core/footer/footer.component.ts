e18e0c7f9abe81ea03cb3ac2df94d2db62649aca
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-22 10:49:59 -0300

    Footer Component

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from '../user/user';
+import { UserService } from '../user/user.serrvice';
+
+@Component({
+	selector: 'ap-footer',
+	templateUrl: './footer.component.html'
+})
+export class FooterComponent implements OnInit {
+
+  user$: Observable<User>
+
+	constructor(private userService: UserService) { }
+
+	ngOnInit() {
+    this.user$ = this.userService.getUser();
+	}
+
+}
