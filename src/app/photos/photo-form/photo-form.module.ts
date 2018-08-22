3f23c3c3a4625ddd33aab60152e457c96a3d5254
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-22 11:04:53 -0300

    Upload Form

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        VMessageModule
     ]
 })
 export class PhotoFormModule { }
