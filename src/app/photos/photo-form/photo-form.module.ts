74ff1507e7143781841f8d4113af36a4c4ae6a32
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-23 14:33:30 -0300

    Preview de imagens

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 
 import { PhotoFormComponent } from './photo-form.component';
 import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
     declarations: [
@@ -15,7 +16,8 @@ import { VMessageModule } from '../../shared/components/vmessage/vmessage.module
         ReactiveFormsModule,
         FormsModule,
         VMessageModule,
-        RouterModule
+        RouterModule,
+        PhotoModule
     ]
 })
 export class PhotoFormModule { }
