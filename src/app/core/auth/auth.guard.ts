9f4dcb9136dd35dabc5ec1ed8772b96b7b7576f2
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-23 16:03:52 -0300

    Bloqueando acesso não autenticado

---
+++ src/app/core/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+import { UserService } from '../user/user.serrvice';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+
+    if (!this.userService.isLogged()) {
+      this.router.navigate([''])
+      return false;
+    }
+    return true;
+  }
+}
