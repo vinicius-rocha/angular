6a8999958b533adcc707a3b2ff888555ee30fe32
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-24 17:39:32 -0300

    Componetizando os comentários

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -13,13 +13,12 @@ import { PhotoComment } from '../photo/photo-comment';
 export class PhotoDetailsComponent implements OnInit {
 
     photo$: Observable<Photo>;
-    comments$: Observable<PhotoComment[]>;
+    photoId: number;
 
     constructor(private route: ActivatedRoute, private photoService: PhotoService) { }
 
     ngOnInit(): void {
-        const photoId = this.route.snapshot.params.photoId;
-        this.photo$ = this.photoService.findById(photoId);
-        this.comments$ = this.photoService.getComments(photoId);
+        this.photoId = this.route.snapshot.params.photoId;
+        this.photo$ = this.photoService.findById(this.photoId);
     }
 }
--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from '../photo/photo.module';
+import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
 
 @NgModule({
     declarations: [
-        PhotoDetailsComponent
+        PhotoDetailsComponent,
+        PhotoCommentsComponent
     ],
     exports: [
-        PhotoDetailsComponent
+        PhotoDetailsComponent,
+        PhotoCommentsComponent
     ],
     imports: [
         PhotoModule,
-        CommonModule
+        CommonModule,
+        RouterModule
     ]
 })
 export class PhotoDetailsModule { }
