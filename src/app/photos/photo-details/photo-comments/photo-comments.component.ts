950694432c6812356a904e2d2d820ea70fdcb90a
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-24 18:32:55 -0300

    Salvando os comentários

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -28,4 +28,14 @@ export class PhotoCommentsComponent implements OnInit {
             ]
         });
     }
+
+    save() {
+        const comment = this.commentForm.get('comment').value as string;
+        this.photoService
+            .addComment(this.photoId, comment)
+            .subscribe(() => {
+                this.commentForm.reset();
+                alert('Comentário adicionado com sucesso')
+            });
+    }
 }
