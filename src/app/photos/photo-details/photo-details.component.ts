8ccca6619d0dd501936d52a9e250bd97c8271f00
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-24 14:32:35 -0300

    Buscando os comentários da API

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -4,18 +4,22 @@ import { Observable } from 'rxjs';
 
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { PhotoComment } from '../photo/photo-comment';
 
 @Component({
     templateUrl: './photo-details.component.html',
     styleUrls: ['./photo-details.component.css']
 })
-export class PhotoDetailsComponent implements OnInit{
+export class PhotoDetailsComponent implements OnInit {
 
     photo$: Observable<Photo>;
+    comments$: Observable<PhotoComment[]>;
+
     constructor(private route: ActivatedRoute, private photoService: PhotoService) { }
-    
+
     ngOnInit(): void {
-        this.photo$ = this.photoService
-            .findById(this.route.snapshot.params.photoId)
+        const photoId = this.route.snapshot.params.photoId;
+        this.photo$ = this.photoService.findById(photoId);
+        this.comments$ = this.photoService.getComments(photoId);
     }
 }
