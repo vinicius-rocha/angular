2da2bbb565db79d9b61ab818f40de936fd06c700
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-25 16:36:31 -0300

    Diferenciando o ambiente de deploy

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 
 import { UserService } from '../user/user.service';
+import { environment } from '../../../environments/environment';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = environment.API_URL;
 
 @Injectable({
   providedIn: 'root'
