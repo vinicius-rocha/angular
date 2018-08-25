4a651041fadf9b287d42895bba009edfb176bfe7
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-25 14:58:02 -0300

    Preparando o serviço para like das fotos

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, catchError } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
 
 import { Photo } from './photo';
 import { PhotoComment } from './photo-comment';
@@ -42,5 +44,14 @@ export class PhotoService {
 
     removePhoto(photoId: number) {
         return this.http.delete(API + '/photos/' + photoId);
+    }
+
+    like(photoId: number) {
+        return this.http
+            .post(API + '/photos/' + photoId + '/like', {}, { observe: 'response' })
+            .pipe(map(res => true))
+            .pipe(catchError(err =>{
+                return err.status == '304' ? of(false) : throwError(err)
+            }));
     }
 }
