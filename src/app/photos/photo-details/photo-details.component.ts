62bb719fe01e6bbb059d3047268074fe7949f8f6
vinicius-rocha <r.vinicius.09@gmail.com>
2018-08-25 14:02:37 -0300

    Melhoria para rotas não encontradas

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -26,6 +26,10 @@ export class PhotoDetailsComponent implements OnInit {
     ngOnInit(): void {
         this.photoId = this.route.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+        this.photo$.subscribe(() => {}, error => {
+            console.log(error);
+            this.router.navigate(['not-found']);
+        })
     }
 
     remove() {
